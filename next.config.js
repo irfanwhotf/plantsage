eb864ae50b88ac6d8537d0e157793c61c7011c40
irfanwhotf <mohammediirfan2006@gmail.com>
2025-01-18 21:33:29 +0530

    Add Wrangler config and update Next.js and TypeScript configs for Cloudflare

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     unoptimized: true,
   },
   env: {
     GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
   },
+  experimental: {
+    runtime: 'edge',
+  }
 }
 
 module.exports = nextConfig
